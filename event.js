708adea3e6473ebbcc728a32caf387d48d9ce548
prabinnnn <prabinbhandari120@gmail.com>
2024-01-19 12:55:21 +0545

    add event

--- event.js
+++ event.js
@@ -6,4 +6,7 @@ const myEmitter = new MyEmitter();
 myEmitter.on("motor", () => {
   console.log("please trun off it");
 });
+setTimeout(() => {
+  console.log("please trun off it");
+}, 3000);
 myEmitter.emit("event");
