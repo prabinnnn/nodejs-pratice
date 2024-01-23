9d8da39967033d944a45830cb68fbb43e5b2c522
prabinnnn <prabinbhandari120@gmail.com>
2024-01-23 21:26:28 +0545

    add longest

---
+++ longest.js
@@ -0,0 +1,14 @@
+const longestWord = (string) => {
+  const words = string.split(" ");
+  let longest = "";
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longest.length) {
+      longest = words[i];
+    }
+  }
+
+  return longest;
+};
+
+console.log(longestWord("Web Development Tutorial"));
